15770961b4f2ccf2717a2bdec48104fc3fa90102
isaiahrivera21 <isaiahcooperdump@gmail.com>
2024-10-04 09:23:50 -0400

    Fix enviornment on starting states. Restrict its movemenets to only going fowards

---
+++ A2/mc_off_policy.py
@@ -0,0 +1,195 @@
+from typing import Any, Literal, Union, List
+
+import numpy as np
+import matplotlib.pyplot as plt 
+import pickle
+from tqdm import trange
+from numpydantic import NDArray, Shape
+from pydantic import BaseModel, ConfigDict, Field
+
+
+from racetrack import EnviornmentA,State,RaceTrack
+
+class EpsilonSoftPolicy(BaseModel):
+    epsilon: float = Field(default=0.1, description="Defines probability we preform an exploratory action") 
+    num_actions : int 
+    target_policy : NDArray
+
+    def policy(self,S):
+        rand_val = np.random.rand()
+        # greedy action 
+        if rand_val > self.epsilon:
+            return self.target_policy[S], (1 - self.epsilon + self.epsilon / self.num_actions)
+
+        else:
+            a = np.random.choice(self.num_actions)
+            if (a == self.target_policy[S]):
+                return a, (1 - self.epsilon + self.epsilon / self.num_actions)
+            else:
+                return a , (self.epsilon / self.num_actions)
+
+# random seed should go here??
+class OffPolicyMC(BaseModel):
+    total_epsiodes : int
+    l : float # lambda
+    Q : NDArray # num of states == width x height of track 
+    C : NDArray
+    target : NDArray
+    env : EnviornmentA
+    behavior: EpsilonSoftPolicy = Field(default=None,description="behavioral policy")
+    
+
+    def __init__(self, **data: Any) -> None:
+        super().__init__(**data)
+        self.behavior = EpsilonSoftPolicy(l = 0.1,
+                                          num_actions=9,
+                                          target_policy=self.target)
+
+    def state_index(self, state: State):
+        # Both velocity components are between 0 and 4. So [0,1,2,3,4] [0,1,2,3,4]
+        # We have track_width*track_hegiht potential spaces the car can be in. (Including the ones that are zero) (most won't be populated).
+        x,y = state.position
+        vx,vy = state.velocity
+        return y * self.env.trackA.shape[1] * 25 + x * 25 + vy * 5 + vx # not sure about this 
+
+    def _update_G(self,G,r):
+        return self.l*G + r
+
+    def _update_C(self,C,W):
+        return C + W # problem 1: Sliding in ALL OF C. SHOULD ONLY BE ONE
+
+    def _update_Q(self,Q,W,C,G):
+        return Q + (W/C)*(G - Q)  # think I'm also updating all of Q. 
+
+    def _update_W(self,W,prob):
+        return W*(1/prob)
+
+    def episode(self,b,noise=True):
+        terminate = False 
+        state = self.env._reset_pos() 
+
+        action, p = b(self.state_index(state))
+        total_reward = 0
+        hist = []
+
+        while not terminate:
+            observation, reward, terminate = self.env.step(a = action,state = state)
+            # updates
+            total_reward += reward
+            state = observation
+            action, p = b(self.state_index(state))
+            hist.append((state,action,reward,p)) 
+
+            # start debugging this 
+            # print("Step Summary",(state,action,reward,p))
+            
+
+        return hist,total_reward
+
+    def off_policy_mc_control(self):
+        reward_hist = []
+        for _ in trange(self.total_epsiodes, desc="Episodes"):
+            b = self.behavior.policy
+            t,total_reward = self.episode(b,True)
+            G = 0
+            W = 1
+            reward_hist.append(total_reward)
+            while t:
+                state,action,reward,p = t.pop()
+                G = self.l*G + reward
+                self.C[self.state_index(state)][action] = self.C[self.state_index(state)][action] + W
+                self.Q[self.state_index(state)][action] = self.Q[self.state_index(state)][action] + (W/self.C[self.state_index(state)][action]) * G - self.Q[self.state_index(state)][action]
+                # choose a new state based on argmaxing something
+                self.target[self.state_index(state)] = np.argmax(self.Q[self.state_index(state)]) # actions will be stored (or the number that corresponds to an action)
+                if action != self.target[self.state_index(state)] : break
+                W = W * (1 / p)
+        return self.target,reward_hist
+
+def main():
+    train = True
+
+    
+    rt = RaceTrack(trackA=np.ones((32,17), dtype=float),trackB=np.ones((32,32), dtype=float))
+    track_a = rt.generate_track_a()
+    env = EnviornmentA(trackA=track_a) 
+    
+
+    nA = 9
+    nS = env.trackA.shape[0] * env.trackA.shape[1] * 25
+
+    total_ep = 100000
+    l = 0.9 # lambda (discount)
+    Q = np.zeros((nS, nA))
+    # Q = np.full((nS, nA), 500.0)
+    C = np.zeros_like(Q)
+    pi = np.argmax(Q, axis=1)
+
+    mc = OffPolicyMC(total_epsiodes=total_ep,
+                    l = l,
+                    Q = Q,
+                    C = C,
+                    target= pi,
+                    env = env)
+    if train:    
+        target_policy,reward_hist = mc.off_policy_mc_control()
+        filename = '100k_mc_results.pkl'
+
+        # Use pickle to save the results
+        with open(filename, 'wb') as file:
+            pickle.dump((target_policy, reward_hist), file)
+
+        print("DONE TRAINING")
+    else:
+
+        np.set_printoptions(threshold = np.inf)
+        filename = '100k_mc_results.pkl'
+        with open(filename, 'rb') as file:
+            n_target_policy, n_reward_hist = pickle.load(file)
+
+        print(n_target_policy)
+        # print(n_reward_hist)
+        plt.figure()
+        plt.plot(range(1, total_ep + 1), n_reward_hist)  # Start range from 1 to avoid log(0)
+        plt.xscale('log')
+        plt.show()
+
+        # we don't care about most of this we just want to run one episode
+    for i in range(1):
+        rt2 = RaceTrack(trackA=np.ones((32,17), dtype=float),trackB=np.ones((32,32), dtype=float))
+        track_a2 = rt2.generate_track_a()
+        env2 = EnviornmentA(trackA=track_a2) 
+        terminate = False 
+        state = env2._reset_pos() 
+        action = target_policy[mc.state_index(state)]
+
+        while not terminate:
+            observation, reward, terminate = env2.step(a = action,state = state)
+            state = observation
+            action = target_policy[mc.state_index(state)]
+            print(state,action)
+        
+
+        #     ax = plt.subplot(2, 5, i + 1)
+        #     ax.axis('off')
+        # ax.imshow(track_a2, cmap='GnBu')
+           
+
+        
+            
+
+
+
+
+
+
+
+
+    
+
+if __name__=="__main__":
+    main()
+
+# probs (so state is only 2 things??)
+
+
+
---
+++ A2/racetrack.py
@@ -0,0 +1,151 @@
+# Generate a RaceTrack
+from typing import Any, Literal, Union, List, NamedTuple
+
+import numpy as np
+from numpydantic import NDArray, Shape
+from pydantic import BaseModel, ConfigDict, Field
+from pydantic_settings import BaseSettings
+import itertools
+
+import matplotlib.pyplot as plt
+
+START= 0.8 
+FINISH= 0.4 
+
+# Do not make a policy that applies for both race tracks 
+class RaceTrack(BaseModel):
+
+    trackA : NDArray[Shape["32, 17"],float]
+    trackB : NDArray[Shape["32, 32"],float]
+
+    def generate_track_a(self):
+        self.trackA[14:, 0] =   0  #[14 and onwards in the first column]
+        self.trackA[21:,1]  =   0
+        self.trackA[29:,2]  =   0
+        self.trackA[7:,9] =   0
+        self.trackA[6:,10:17] =   0
+        self.trackA[0:3,0] = 0
+        self.trackA[0:2,1] = 0
+        self.trackA[0:1,2] = 0
+        self.trackA[0,3] = 0
+
+        # Distinguish between Start and Finish
+        self.trackA[31,3:9] = START
+        self.trackA[0:6,16] = FINISH
+
+        return self.trackA
+    def generate_track_b():
+        pass
+
+class State(NamedTuple):
+    position: np.ndarray
+    velocity: np.ndarray
+
+
+# this can probs work for both envs. Just need to make a slight change
+class EnviornmentA(BaseModel):
+    trackA : NDArray[Shape["32, 17"],float]
+    actions : NDArray = Field(default_factory=lambda: np.array(list(itertools.product([-1, 0, 1], [-1, 0, 1]))), description="All 9 actions the car can take")
+    starting_states: NDArray = Field(default_factory=lambda: np.array([]), description="Starting squares of the car")
+    ending_states: NDArray = Field(default_factory=lambda: np.array([]), description="Ending squares of the car")
+    # should add something about the number of states
+
+    noise: float = Field(default=0.1, description="Probability that we do not update our speed")
+    
+    def __init__(self, **data: Any) -> None:
+        super().__init__(**data)
+        self.starting_states = np.dstack(np.where(self.trackA == START))[0]
+        self.ending_states = np.dstack(np.where(self.trackA == FINISH))[0]
+    
+    def _vel_check(self,curr_vel,accel):
+        next_vel = np.clip(curr_vel + accel, a_min=0, a_max=4)
+        if np.sum(next_vel) == 0:
+            next_vel = curr_vel
+            return next_vel
+        return next_vel
+    
+
+    def _finish_check(self,proj_pos):
+
+        if(self.trackA[proj_pos[0],proj_pos[1]] == FINISH):
+            # print("FINISHED BOIIII")
+            return True
+        
+        return False 
+    
+    # check if the vehicle hit the wall. If it does then reset 
+    def _accident_check(self,proj_pos):
+        proj_y = proj_pos[0]
+        proj_x = proj_pos[1] #think we just check if the value of the state is equal to 0
+        if (proj_y < 0) or (proj_x > 16): return True
+        if(self.trackA[proj_y,proj_x] == 0.0):
+            # print("ACCIDENT AHHHHHH")
+            # problem child function
+            return True
+
+    
+    def _reset_pos(self):
+        # choose something on the random starting state
+        start_idx = np.random.choice(self.starting_states.shape[0])
+        return State(self.starting_states[start_idx],np.array([0,0]))
+
+    def step(self,a,state: State):
+        # need to get the current state
+        
+        reward = -1
+        terminate = False
+
+        position, velocity = state
+
+        # decide if noise will intefere or not (2 should be changed)
+        # if  np.random.rand() <= 0.1: 
+        #     # give us the action where we do not accelerate actions[4]
+        #     accel = self.actions[4]
+        # else:
+        #     accel = self.actions[a]
+        accel = self.actions[a]
+
+        # check that the velocity follows the guidlines 
+        
+        next_vel = self._vel_check(velocity,accel)
+
+
+        
+        next_pos = np.array([position[0]-next_vel[0],position[1]+next_vel[1]])
+        # something here needs to negative 
+
+        
+        
+        # if finish check, if accident_check, else next_pos = next_vel + pos or smth
+        if self._accident_check(next_pos):
+            return self._reset_pos(),reward,terminate
+        elif self._finish_check(next_pos):
+            terminate = True
+            return State(next_pos,next_vel),reward,terminate # reward might need to be 0? 
+        else:
+            return State(next_pos,next_vel),reward,terminate
+        
+        
+
+def main():
+
+    rt = RaceTrack(trackA=np.ones((32,17), dtype=float),trackB=np.ones((32,32), dtype=float))
+    track_a = rt.generate_track_a()
+
+    plt.figure(figsize=(10, 5))
+    plt.imshow(track_a,cmap='Pastel1_r')
+    plt.show()
+
+    env= EnviornmentA(trackA=track_a)
+    print(env.starting_states)
+    print(env.ending_states)
+    breakpoint()
+
+
+if __name__=="__main__":
+    main()
+     
+
+
+
+
